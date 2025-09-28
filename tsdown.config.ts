96ef47c362ecde1d41e7d88195ea570dbe5efaf5
silverwind <me@silverwind.io>
2025-09-29 00:53:45 +0200

    reduce

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,6 @@
 import {fileURLToPath} from "node:url";
 import {type UserConfig} from "tsdown";
 
-const entry = fileURLToPath(new URL("index.ts", import.meta.url));
-console.info(entry);
-console.info(JSON.stringify(entry));
-
 export default {
-  entry,
+  entry: fileURLToPath(new URL("index.ts", import.meta.url)),
 } satisfies UserConfig;
