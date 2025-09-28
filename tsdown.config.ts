0e02e14178ba5dba1c08d8b9143d912504fac12f
silverwind <me@silverwind.io>
2025-09-29 00:46:27 +0200

    debug

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,8 @@
 import {fileURLToPath} from "node:url";
 import {type UserConfig} from "tsdown";
 
-const entry = fileURLToPath(new URL("index.ts", import.meta.url)).replaceAll("\\\\", "\\");
+const entry = fileURLToPath(new URL("index.ts", import.meta.url));
+console.info(entry);
 console.info(JSON.stringify(entry));
 
 export default {
