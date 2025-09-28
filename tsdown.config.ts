85f4f4904e7e349fded33ddc93aafbf35221c6d5
silverwind <me@silverwind.io>
2025-09-29 01:12:44 +0200

    replace slashes on windows

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,12 @@
 import {fileURLToPath} from "node:url";
+import {platform} from "node:os";
 import {type UserConfig} from "tsdown";
 
+let entry = fileURLToPath(new URL("index.ts", import.meta.url));
+if (platform() === "win32") {
+  entry = entry.replaceAll("\\", "/"); // https://tsdown.dev/options/entry#using-glob-patterns
+}
+
 export default {
-  entry: fileURLToPath(new URL("index.ts", import.meta.url)),
+  entry,
 } satisfies UserConfig;
