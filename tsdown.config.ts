41e7efb35ec196d3428510c0bde6dc213e1fe179
silverwind <me@silverwind.io>
2025-09-29 07:16:23 +0200

    try glob

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,11 @@
 import {fileURLToPath} from "node:url";
-import {platform} from "node:os";
+import {globSync} from "node:fs";
 import {type UserConfig} from "tsdown";
 
 let entry = fileURLToPath(new URL("index.ts", import.meta.url));
-if (platform() === "win32") {
-  entry = entry.replaceAll("\\", "/"); // https://tsdown.dev/options/entry#using-glob-patterns
-}
+console.info(`before glob: ${entry}`);
+entry = globSync(entry);
+console.info(`after glob: ${entry}`);
 
 export default {
   entry,
